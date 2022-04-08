1d168cc8ff253df6c67f51d3587d2322a0e574b9
Robert Arturo Jaime Nieto <102542899+RobertJaime@users.noreply.github.com>
2022-04-08 12:05:25 -0500

    Add files via upload

---
+++ TALLER ALGORITMOS/MULTIPLO DE 3.py
@@ -0,0 +1,6 @@
+numero=int(input("Ingrese un numero: "))
+#que el numero sea impar y multiplo de 3
+if(numero%2==1 and numero%3==0):
+    print("el numero es impar y multiple de tres:",numero)
+else:
+        print("el numero debe ser impar y multiplo de tres")
